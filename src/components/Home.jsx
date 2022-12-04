a6bc69b3dc5b883f279f9b27e67bc1c3b5c3a249
bkmahapatra <bkmahapatra27@gmail.com>
2022-12-04 20:30:40 +0530

    banner fix1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import "../styles/Home.css";
 import About from "../components/About";
 import Clients from "../components/Clients";
@@ -8,29 +8,32 @@ import Services from "../components/Services";
 import Teams from "../components/Teams";
 import { BsArrowDownCircle } from "react-icons/bs";
 import Contact from "./Contact";
-import HarvisVdo from '../resources/Harvis.mp4';
+import HarvisVdo from "../resources/Harvis.mp4";
 
 const Home = () => {
-  useEffect(()=>{
-    window.addEventListener("scroll",()=>{
-      if(document.documentElement.scrollTop+window.innerHeight+1 >= document.querySelector(".home_body").scrollHeight){
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.documentElement.scrollTop + window.innerHeight + 1 >=
+        document.querySelector(".home_body").scrollHeight
+      ) {
         document.querySelector(".Nav_body").classList.add("white_bg");
         document.querySelector(".nav_item a").classList.add("white_bg_text");
 
-        console.log("oh yes")
-      }
-      else{
+        console.log("oh yes");
+      } else {
         document.querySelector(".Nav_body").classList.remove("white_bg");
-
       }
-    })
-  })
+    });
+  });
   return (
     <>
       <div className="home_body">
         <video autoPlay loop muted id="page_vdo">
           <source src={HarvisVdo} type="video/mp4" />
         </video>
+        <div className="banner_gradient"></div>
+
         <div className="home_headline">Lorem ipsum dolor sit amet.</div>
         <div className="home_btn btn_know_more">Know More</div>
         <div className="home_down_arr">
